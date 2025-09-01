894f835b7f4651eef1c3b018ae9b873e06b4b3a2
Dawid Davtyan <dawiddavtyan@gmail.com>
2025-09-01 11:34:56 +0200

    Initial

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,6 +1,20 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
 app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
 
 app.Run();
