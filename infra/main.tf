96550abdf4c15fbdd93b4899c0724295847c1c21
Dawid Davtyan <dawiddavtyan@gmail.com>
2025-09-10 09:49:20 +0200

    (feat) : Adding AppInsights to ResourceGroup

--- infra/main.tf
+++ infra/main.tf
@@ -8,3 +8,11 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.project_name}-${var.environment}-rg"
   location = var.location
 }
+
+# Application Insights
+resource "azurerm_application_insights" "appinsights" {
+  name                = "${var.project_name}-${var.environment}-ai"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
