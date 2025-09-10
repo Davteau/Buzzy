352da9ac179478636da820bf28dedf3ca1bc6f16
Dawid Davtyan <dawiddavtyan@gmail.com>
2025-09-10 11:53:08 +0200

    (feat) : Adding KeyVault

--- infra/main.tf
+++ infra/main.tf
@@ -29,3 +29,13 @@ resource "azurerm_postgresql_flexible_server" "db" {
   version                = "13"
   backup_retention_days  = 7
 }
+
+# Key Vault
+resource "azurerm_key_vault" "kv" {
+  name                = "${var.project_name}${var.environment}kv"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+  purge_protection_enabled = false
+}
