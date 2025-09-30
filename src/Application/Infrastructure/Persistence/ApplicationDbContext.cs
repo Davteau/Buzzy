d1cf8aa0d53895f4e7a91d19c791f9689e705d24
Dawid Davtyan <dawiddavtyan@gmail.com>
2025-09-30 15:04:10 +0200

    fix: enums fixed to strings instead of integers

--- src/Application/Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/Application/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -13,5 +13,16 @@ namespace Application.Infrastructure.Persistence
         public DbSet<OfferingCategory> OfferingCategories { get; set; }
         public DbSet<EmploymentOffering> EmploymentOfferings { get; set; }
         public DbSet<InvitationLink> InvitationLinks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(u => u.Role)
+                .HasConversion<string>();
+
+            modelBuilder.Entity<Appointment>()
+                .Property(a => a.Status)
+                .HasConversion<string>();
+        }
     }
 }
